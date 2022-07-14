49275df2b284f6f177851a8b4de00d06e0c56ab8
Vishnu Mukundan <vishnumukundan040@gmail.com>
2022-07-14 23:01:55 +0530

    first commit

---
+++ lib/config/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+
+const kColorWhite = Color(0xffffffff);
+const kColorBlack = Color(0xff000000);
---
+++ lib/config/values.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+
+///
+/// size values
+const double kSizeBase = 8;
+
+const double kSizeSmall = 8;
+const double kSizeNormal = 16;
+const double kSizeMedium = 24;
+const double kSizeLarge = 32;
+const double kSizeExtraLarge = 64;
+
+///
+/// spacing values
+const double kBasePadding = 16;
+
+const double kPaddingSmall = 8;
+const double kPaddingNormal = 16;
+const double kPaddingMedium = 24;
+const double kPaddingLarge = 32;
+const double kPaddingExtraLarge = 64;
+
+///
+/// sized box spacing
+const kSizedWidth = SizedBox(width: kBasePadding);
+const kSizedHeight = SizedBox(height: kBasePadding);
+
+const kSizedWidthSmall = SizedBox(width: kPaddingSmall);
+const kSizedHeightSmall = SizedBox(height: kPaddingSmall);
+
+const kSizedWidthNormal = SizedBox(width: kPaddingNormal);
+const kSizedHeightNormal = SizedBox(height: kPaddingNormal);
+
+const kSizedWidthMedium = SizedBox(width: kPaddingNormal);
+const kSizedHeightMedium = SizedBox(height: kPaddingNormal);
+
+const kSizedWidthLarge = SizedBox(width: kPaddingLarge);
+const kSizedHeightLarge = SizedBox(height: kPaddingLarge);
+
+const kSizedWidthExtraLarge = SizedBox(width: kPaddingExtraLarge);
+const kSizedHeightExtraLarge = SizedBox(height: kPaddingExtraLarge);
+
+///
+/// 
